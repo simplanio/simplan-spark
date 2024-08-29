160daf12b79ece3f1f7933781b07fc3517e53612
tabraham1 <thomas_abraham@intuit.com>
2024-08-29 08:59:36 -0700

    Ability to start from a task

--- spark-core/src/main/scala/com/intuit/data/simplan/spark/core/operators/transformations/SqlStatementOperator.scala
+++ spark-core/src/main/scala/com/intuit/data/simplan/spark/core/operators/transformations/SqlStatementOperator.scala
@@ -3,6 +3,7 @@ package com.intuit.data.simplan.spark.core.operators.transformations
 import com.intuit.data.simplan.core.domain.operator.OperatorContext
 import com.intuit.data.simplan.core.domain.operator.config.transformations.SqlStatementConfig
 import com.intuit.data.simplan.core.domain.{Lineage, TableType}
+import com.intuit.data.simplan.logging.Logging
 import com.intuit.data.simplan.spark.core.context.SparkAppContext
 import com.intuit.data.simplan.spark.core.domain.{SparkOperatorRequest, SparkOperatorResponse}
 import com.intuit.data.simplan.spark.core.operators.SparkOperator
@@ -11,9 +12,13 @@ import org.apache.spark.sql.DataFrame
 /** @author - Abraham, Thomas - tabaraham1
   *         Created on 8/19/21 at 3:01 PM
   */
-class SqlStatementOperator(appContext: SparkAppContext, operatorContext: OperatorContext) extends SparkOperator[SqlStatementConfig](appContext, operatorContext) {
+class SqlStatementOperator(appContext: SparkAppContext, operatorContext: OperatorContext) extends SparkOperator[SqlStatementConfig](appContext, operatorContext) with Logging {
 
   override def process(request: SparkOperatorRequest): SparkOperatorResponse = {
+
+    logger.info(s"Executing SQL Statement Operator : ${operatorContext.taskName} : ${operatorConfig.sql}")
+
+
     val frame: DataFrame = appContext.spark.sql(operatorConfig.sql)
     operatorConfig.tableType match {
       case TableType.TEMP    => frame.createOrReplaceTempView(operatorConfig.table.getOrElse(operatorContext.taskName))
