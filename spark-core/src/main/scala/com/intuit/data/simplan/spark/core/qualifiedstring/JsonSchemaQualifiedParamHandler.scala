0e702bb56bf5362b8a9c8ecd3670b6c2270a9673
tabraham1 <thomas_abraham@intuit.com>
2025-02-25 16:34:13 -0800

    LCA-41 - Updated Simplan Schema for Ops Metrics

--- spark-core/src/main/scala/com/intuit/data/simplan/spark/core/qualifiedstring/JsonSchemaQualifiedParamHandler.scala
+++ spark-core/src/main/scala/com/intuit/data/simplan/spark/core/qualifiedstring/JsonSchemaQualifiedParamHandler.scala
@@ -9,6 +9,7 @@ import org.apache.spark.sql.types.{DataType, StructType}
 
 import java.io.File
 import java.net.URI
+import scala.util.Try
 
 /** @author Abraham, Thomas - tabraham1
   *         Created on 26-Apr-2022 at 2:34 PM
@@ -18,11 +19,17 @@ class JsonSchemaQualifiedParamHandler extends QualifiedParamHandler[StructType] 
 
   override def resolve(qualifiedString: QualifiedParam): StructType = {
     logger.info(s"Resolving JsonSchemaQualifiedParamHandler for ${qualifiedString.string}")
+    val schemeSpecificPart = new URI(qualifiedString.string).getSchemeSpecificPart
     val jsonSchemaDefinition = qualifiedString.string match {
-      case r if r.startsWith("classpath:") => ConfigFactory.parseResources(new URI(qualifiedString.string).getSchemeSpecificPart).root().render(ConfigRenderOptions.concise())
-      case r if r.startsWith("file:")      => ConfigFactory.parseFile(new File(new URI(qualifiedString.string).getSchemeSpecificPart)).root().render(ConfigRenderOptions.concise())
+      case r if r.startsWith("classpath:") => ConfigFactory.parseResources(schemeSpecificPart).root().render(ConfigRenderOptions.concise())
+      case r if r.startsWith("file:")      => ConfigFactory.parseFile(new File(schemeSpecificPart)).root().render(ConfigRenderOptions.concise())
       case r                               => r
     }
-    DataType.fromJson(jsonSchemaDefinition).asInstanceOf[StructType]
+    Try(DataType.fromJson(jsonSchemaDefinition).asInstanceOf[StructType]) match {
+      case scala.util.Success(value) => value
+      case scala.util.Failure(exception) =>
+        logger.error(s"Failed to parse json schema for ${qualifiedString.qualifiedString} - Json Schema Definition: $jsonSchemaDefinition", exception)
+        throw exception
+    }
   }
 }
